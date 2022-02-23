6e5546dc4da2abb77a0260c6529e5bffa8e192aa
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-02-23 16:03:43 +0100

    chore: creating of the project's README

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,46 +2,50 @@
 <template>
   <div class="pv4 ph3 bg-washed-yellow h-100 mv3 mh2 br2">
     <div class="flex flex-wrap justify-between-l justify-center">
-      <a v-for="image in images" :key="image.id" class="w-100 w-40-m w-30-l db center tc black link dim ma2" href="">
+      <a v-for="image in imageList" :key="image.id" class="w-100 w-40-m w-30-l db center tc black link dim ma2" href="#">
         <img class="db center ba b--black-10 ma2" :src="image.urls.small" :alt="image.alt_description" >
         <dl class="mt2 f6 lh-copy">
           <dt class="clip">Artist</dt>
           <dd class="ml0 gray">{{image.user.name}}</dd>
         </dl>
       </a>
+      <Observer @intersect="intersected"/>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+import Observer from "@/components/Observer";
 
 export default {
-  data: () => ({
-    images: []
-  }),
-  mounted(){
-    this.searchUnsplash()
+  components: {
+    Observer
   },
+  data: () => ({
+    imageList: [],
+    page: 1
+  }),
   methods: {
-    searchUnsplash() {
-      this.images = [];
+    intersected() {
       axios
         .get(
-          `https://api.unsplash.com/photos?per_page=21&w=1280&h=1280`,
+          `https://api.unsplash.com/photos?page=${this.page}&per_page=10&w=1280&h=1280`,
           {
             headers: {
               Authorization:
-                "Client-ID 2IslnBxGaSAz7MpV89-REHyWsFYvt_Id875LcGpVO1o",
+                "Client-ID {add your client-ID here}",
               "Accept-Version": "v1"
             }
           }
         )
         .then(response => {
-          this.images = response.data;
+          this.page++;
+          const lists = response.data;
+          this.imageList = [...this.imageList, ...lists];
         })
         .catch(() => {
-          this.images = [];
+          this.imageList = [];
       });
     }
   }
